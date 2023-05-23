52b43613bc6d16930f5add57dbc03510170015dd
Harrison <imthatguyh9@gmail.com>
2023-05-23 15:24:44 +1200

    final 1.1

--- assign2/admin.js
+++ assign2/admin.js
@@ -1,52 +1,52 @@
-<<<<<<< HEAD
-/**
- * The function searches for a booking using a booking reference number and displays the search
- * results.
- * @param {Event} e - The event parameter is an object that represents the event that triggered the
- * function. In this case, it is the "submit" event of a form.
- */
+window.onload = searchEmptyBooking;
+
 function searchBooking(e) {
   if (e) e.preventDefault();
   const searchInput = document.getElementById("bsearch").value;
   // Check if search input is empty
   if (searchInput.trim() === "") {
-    return; // Exit the function
+    searchEmptyBooking();
+  } else {
+    searchBookingByReference(searchInput);
   }
-  // Validate the booking reference number format
-  if (!/^BRN\d{5}$/.test(searchInput)) {
-    alert("Invalid booking reference number format. Example: BRN00001");
-    return; // Exit the function
-  }
-  // Create an XMLHttpRequest object
+}
+
+function searchEmptyBooking() {
   const xhr = new XMLHttpRequest();
-  // Set up the request
-  xhr.open("GET", "admin.php?bsearch=" + searchInput, true);
-  // Define the callback function
+  xhr.open("GET", "admin.php?bsearch=", true);
   xhr.onreadystatechange = function () {
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
       const response = JSON.parse(xhr.responseText);
       displaySearchResults(response);
     }
   };
-  // Send the request
   xhr.send();
 }
 
-/**
- * The function displays search results in a table format with booking details and an assign button
- * for each booking.
- * @param {Array} data - The data parameter is an array of objects containing information about bookings.
- * Each object represents a single booking and contains properties such as booking reference number,
- * customer name, phone number, pickup and destination suburbs, pickup date and time, and booking
- * status. The function uses this data to generate an HTML table displaying the search results.
- */
-function displaySearchResults(data) {
-  const contentDiv = document.querySelector(".content");
-  contentDiv.innerHTML = ""; // Clear previous search results
-  if (data.error) {
-    alert(data.error);
-    return; // Exit the function
+function searchBookingByReference(reference) {
+  if (!/^BRN\d{5}$/.test(reference)) {
+    alert("Invalid booking reference number format. Example: BRN00001");
+    return;
   }
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "admin.php?bsearch=" + reference, true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      const response = JSON.parse(xhr.responseText);
+      displaySearchResults(response);
+    }
+  };
+  xhr.send();
+}
+
+function displaySearchResults(data) {
+  const searchInput = document.getElementById("bsearch");
+  const searchValue = searchInput.value;
+  searchInput.value = "";
+
+  const contentDiv = document.querySelector(".content");
+  contentDiv.innerHTML = "";
+
   let html = `
     <table class="admin-table">
       <tr>
@@ -59,169 +59,30 @@ function displaySearchResults(data) {
         <th>Status</th>
         <th>Assign</th>
       </tr>`;
-  data.forEach(function (booking) {
-    const inputDate = new Date(`${booking.pickup_date}T${booking.pickup_time}`);
-    const formattedDate = inputDate.toLocaleDateString("en-NZ", {
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-      hour12: false,
-    });
 
-    let formattedPhone = booking.phone;
-    if (!formattedPhone.includes("-") && formattedPhone.length > 3) {
-      formattedPhone = formattedPhone.slice(0, 3) + "-" + formattedPhone.slice(3);
-    }
-
+  if (data.error) {
     html += `
       <tr>
-        <td>${booking.booking_ref}</td>
-        <td>${booking.cname}</td>
-        <td>${formattedPhone}</td>
-        <td>${booking.snumber} ${booking.stname} ${booking.sbname}</td>
-        <td>${booking.dsbname}</td>
-        <td>${formattedDate.replace(",", "")}</td>
-        <td>${booking.booking_status}</td>
-        <td>
-          <button class="assignBtn" data-ref="${booking.booking_ref}" ${booking.booking_status === "assigned" ? "disabled" : ""
-      }>Assign</button>
-        </td>
+        <td colspan="8">No bookings found.</td>
       </tr>`;
-  });
-  html += `</table>`;
-  contentDiv.innerHTML = html;
-  const assignBtns = document.querySelectorAll(".assignBtn");
-  for (const assignBtn of assignBtns) {
-    assignBtn.addEventListener("click", assignBooking);
-  }
-}
+  } else {
+    data.forEach(function (booking) {
+      const inputDate = new Date(`${booking.pickup_date}T${booking.pickup_time}`);
+      const formattedDate = inputDate.toLocaleDateString("en-NZ", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      });
 
-/**
- * This function assigns a booking by sending a GET request to the server and updating the booking
- * status in the table.
- * @param {Event} event - The event parameter is the event object that is passed to the function when it is
- * called. It contains information about the event that triggered the function, such as the target
- * element and any data associated with it.
- */
-function assignBooking(event) {
-  const bookingRef = event.target.dataset.ref;
-  // Create an XMLHttpRequest object
-  const xhr = new XMLHttpRequest();
-  // Set up the request
-  xhr.open("GET", "admin.php?assign=" + bookingRef, true);
-  // Define the callback function
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-      const response = xhr.responseText;
-      showConfirmation(response);
-      event.target.disabled = true;
-      // Update the booking status in the table
-      const statusCell = event.target.parentNode.previousElementSibling;
-      statusCell.textContent = "assigned";
-    }
-    // if theres a error
-    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 400) {
-      const response = xhr.responseText;
-      showError(response);
-    }
-  };
-  // Send the request
-  xhr.send();
-}
+      let formattedPhone = booking.phone;
+      if (!formattedPhone.includes("-") && formattedPhone.length > 3) {
+        formattedPhone = formattedPhone.slice(0, 3) + "-" + formattedPhone.slice(3);
+      }
 
-/**
- * The function creates a success message element with a given message and inserts it as the first
- * child of a content element.
- * @param message - The message to be displayed in the confirmation div.
- */
-function showConfirmation(message) {
-  const confirmationDiv = document.createElement("div");
-  confirmationDiv.classList.add("success-message");
-  confirmationDiv.textContent = message;
-  const contentDiv = document.querySelector(".content");
-  contentDiv.insertBefore(confirmationDiv, contentDiv.firstChild);
-}
-
-/**
- * The function creates and inserts an error message into the first child of a content div.
- * @param message - The error message that will be displayed in the errorDiv element.
- */
-function showError(message){
-  const errorDiv = document.createElement("div");
-  errorDiv.classList.add("error-message");
-  errorDiv.textContent = message;
-  const contentDiv = document.querySelector(".content");
-  contentDiv.insertBefore(errorDiv, contentDiv.firstChild);
-}
-=======
-document.addEventListener("DOMContentLoaded", function() {
-	const searchForm = document.getElementById("searchForm");
-	const contentDiv = document.querySelector(".content");
-
-	function searchBooking(event) {
-		event.preventDefault();
-		const searchInput = document.getElementById("bsearch").value;
-		// Check if search input is empty
-		if (searchInput.trim() === "") {
-			return; // Exit the function
-		}
-		// Validate the booking reference number format
-		if (!/^BRN\d{5}$/.test(searchInput)) {
-			alert("Invalid booking reference number format. Example: BRN00001");
-			return; // Exit the function
-		}
-		// Create an XMLHttpRequest object
-		const xhr = new XMLHttpRequest();
-		// Set up the request
-		xhr.open("GET", "admin.php?bsearch=" + searchInput, true);
-		// Define the callback function
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-				const response = JSON.parse(xhr.responseText);
-				displaySearchResults(response);
-			}
-		};
-		// Send the request
-		xhr.send();
-	}
-
-	function displaySearchResults(data) {
-		if (data.error) {
-			alert(data.error);
-			return; // Exit the function
-		}
-		let html = `
-      <table class="admin-table">
-        <tr>
-          <th>Booking reference number</th>
-          <th>Customer name</th>
-          <th>Phone</th>
-          <th>Pickup suburb</th>
-          <th>Destination suburb</th>
-          <th>Pickup date and time</th>
-          <th>Status</th>
-          <th>Assign</th>
-        </tr>
-    `;
-		data.forEach(function(booking) {
-			const inputDate = new Date(`${booking.pickup_date}T${booking.pickup_time}`);
-			const formattedDate = inputDate.toLocaleDateString("en-NZ", {
-				day: "2-digit",
-				month: "2-digit",
-				year: "numeric",
-				hour: "2-digit",
-				minute: "2-digit",
-				hour12: false,
-			});
-
-			let formattedPhone = booking.phone;
-			if (!formattedPhone.includes("-") && formattedPhone.length > 3) {
-				formattedPhone = formattedPhone.slice(0, 3) + "-" + formattedPhone.slice(3);
-			}
-
-			html += `
+      html += `
         <tr>
           <td>${booking.booking_ref}</td>
           <td>${booking.cname}</td>
@@ -231,50 +92,67 @@ document.addEventListener("DOMContentLoaded", function() {
           <td>${formattedDate.replace(",", "")}</td>
           <td>${booking.booking_status}</td>
           <td>
-            <button class="assignBtn" data-ref="${booking.booking_ref}" ${booking.booking_status === "assigned" ? "disabled" : ""
-        }>Assign</button>
+            <button class="assignBtn" data-ref="${booking.booking_ref}" ${booking.booking_status === "assigned" ? "disabled" : ""}>Assign</button>
           </td>
-        </tr>
-      `;
-		});
-		html += `</table>`;
-		contentDiv.innerHTML = html;
-		const assignBtns = document.querySelectorAll(".assignBtn");
-		for (const assignBtn of assignBtns) {
-			assignBtn.addEventListener("click", assignBooking);
-		}
-	}
+        </tr>`;
+    });
+  }
 
-	function assignBooking(event) {
-		const bookingRef = event.target.dataset.ref;
-		// Create an XMLHttpRequest object
-		const xhr = new XMLHttpRequest();
-		// Set up the request
-		xhr.open("GET", "admin.php?assign=" + bookingRef, true);
-		// Define the callback function
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-				const response = xhr.responseText;
-				showConfirmation(response);
-				event.target.disabled = true;
-				// Update the booking status in the table
-				const statusCell = event.target.parentNode.previousElementSibling;
-				statusCell.textContent = "assigned";
-			}
-		};
-		// Send the request
-		xhr.send();
-	}
+  html += `</table>`;
+  contentDiv.innerHTML = html;
+  const assignBtns = document.querySelectorAll(".assignBtn");
+  for (const assignBtn of assignBtns) {
+    assignBtn.addEventListener("click", assignBooking);
+  }
 
-	function showConfirmation(message) {
-		const confirmationDiv = document.createElement("div");
-		confirmationDiv.classList.add("success-message");
-		confirmationDiv.textContent = message;
-		contentDiv.insertBefore(confirmationDiv, contentDiv.firstChild);
-	}
-	// Attach the event listener to the form submit event
-	if (searchForm) {
-		searchForm.addEventListener("submit", searchBooking);
-	}
-});
->>>>>>> 1ea1b2c8290a9422a237b3ca86957b9e7f4be0f8
+  searchInput.value = searchValue;
+}
+
+function assignBooking(event) {
+  const bookingRef = event.target.dataset.ref;
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "admin.php?assign=" + bookingRef, true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      const response = xhr.responseText;
+      showConfirmation(response);
+      event.target.disabled = true;
+      const statusCell = event.target.parentNode.previousElementSibling;
+      statusCell.textContent = "assigned";
+    }
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 400) {
+      const response = xhr.responseText;
+      showError(response);
+    }
+  };
+  xhr.send();
+}
+
+function showConfirmation(message) {
+  const confirmationDiv = document.createElement("div");
+  confirmationDiv.classList.add("success-message");
+  confirmationDiv.textContent = message;
+  const contentDiv = document.querySelector(".content");
+  contentDiv.insertBefore(confirmationDiv, contentDiv.firstChild);
+}
+
+function showError(message) {
+  const errorDiv = document.createElement("div");
+  errorDiv.classList.add("error-message");
+  errorDiv.textContent = message;
+  const contentDiv = document.querySelector(".content");
+  contentDiv.insertBefore(errorDiv, contentDiv.firstChild);
+}
+
+function submitSearch(event) {
+  event.preventDefault();
+  const searchInput = document.getElementById("bsearch").value.trim();
+  if (searchInput === "") {
+    searchEmptyBooking();
+  } else {
+    const url = `admin.html?bsearch=${searchInput}`;
+    window.location.href = url;
+  }
+
+  document.getElementById("bsearch").value = ""; // Clear the search field
+}
--- assign2/admin.php
+++ assign2/admin.php
@@ -10,6 +10,7 @@ if ($conn->connect_error) {
 }
 
 // Handle the search request
+// Handle the search request
 if (isset($_GET["bsearch"])) {
     $bsearch = $_GET["bsearch"];
 
@@ -21,21 +22,12 @@ if (isset($_GET["bsearch"])) {
     // Prepare the SQL query
     $sql = "SELECT * FROM BookingRequests WHERE ";
     if (empty($bsearch)) {
-        $sql .= "pickup_date >= CURDATE() AND pickup_time >= CURTIME() AND ";
+        $sql .= "pickup_date >= CURDATE() AND pickup_time BETWEEN CURTIME() AND DATE_ADD(CURTIME(), INTERVAL 2 HOUR) AND booking_status = 'unassigned' ORDER BY pickup_date, pickup_time";
     } else {
-        $sql .= "booking_ref = '$bsearch' AND ";
+        $sql .= "booking_ref = '$bsearch'";
     }
-<<<<<<< HEAD
-    $sql .= "booking_status IN ('unassigned', 'assigned') ORDER BY pickup_date, pickup_time";
 
     // Execute the query
-=======
-    $sql .=
-        "booking_status IN ('unassigned', 'assigned') ORDER BY pickup_date, pickup_time";
-
-    // Execute the query
-    // Execute the query
->>>>>>> 1ea1b2c8290a9422a237b3ca86957b9e7f4be0f8
     $result = $conn->query($sql);
 
     // Check if any results were found
@@ -50,6 +42,7 @@ if (isset($_GET["bsearch"])) {
     }
 }
 
+
 // Handle the assignment request
 if (isset($_GET["assign"])) {
     $assignRef = $_GET["assign"];
@@ -58,13 +51,7 @@ if (isset($_GET["assign"])) {
     $sql = "UPDATE BookingRequests SET booking_status = 'assigned' WHERE booking_ref = '$assignRef'";
 
     if ($conn->query($sql) === true) {
-<<<<<<< HEAD
         echo "Congratulations! Booking request " . $assignRef . " has been assigned!";
-=======
-        echo "Congratulations! Booking request " .
-            $assignRef .
-            " has been assigned!";
->>>>>>> 1ea1b2c8290a9422a237b3ca86957b9e7f4be0f8
     } else {
         echo "Error assigning booking request: " . $conn->error;
     }
