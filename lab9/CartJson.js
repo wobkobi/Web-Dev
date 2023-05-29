2da4bb3295f70d80e2a310ac187e98f7732f7b6b
wobkobi <imthatguyh9@gmail.com>
2023-05-30 04:31:17 +1200

    Task 1 c

--- lab9/CartJson.js
+++ lab9/CartJson.js
@@ -1,48 +1,59 @@
 var xHRObject = false;
 if (window.XMLHttpRequest) {
     xHRObject = new XMLHttpRequest();
-}
-else if (window.ActiveXObject) {
+} else if (window.ActiveXObject) {
     xHRObject = new ActiveXObject("Microsoft.XMLHTTP");
 }
 
-
 function getData() {
-    if ((xHRObject.readyState == 4) && (xHRObject.status == 200)) {
+    if (xHRObject.readyState === 4 && xHRObject.status === 200) {
         var spantag = document.getElementById("cart");
         var serverResponse;
-        if (xHRObject.responseText != "") serverResponse = JSON.parse(xHRObject.responseText);
-        else serverResponse = null;
+        if (xHRObject.responseText !== "") {
+            serverResponse = JSON.parse(xHRObject.responseText);
+        } else {
+            serverResponse = null;
+        }
 
-        if (serverResponse != null) {
-            var cartItems = serverResponse.cart;
+        if (serverResponse !== null) {
+            var cartData = serverResponse.cart;
             var totalCost = serverResponse.totalCost;
+
             spantag.innerHTML = "";
 
-            for (var key in cartItems) {
-                var item = cartItems[key];
-                spantag.innerHTML += "Book: " + key;
-                spantag.innerHTML += " ISBN: " + item.isbn;
-                spantag.innerHTML += " Quantity: " + item.quantity;
-                spantag.innerHTML += " Price: " + item.price + "<br>";
+            if (Object.keys(cartData).length === 0) {
+                spantag.innerHTML = "";
+            } else {
+                for (var key in cartData) {
+                    if (cartData.hasOwnProperty(key)) {
+                        spantag.innerHTML += " " + key + " (ISBN: " + cartData[key].ISBN + ")";
+                        spantag.innerHTML += " Quantity: " + cartData[key].quantity;
+                        spantag.innerHTML += " Price: $" + cartData[key].price;
+                        spantag.innerHTML += " <a href='#' onclick='AddRemoveItem(\"Remove\", \"" + key + "\");'>Remove Item</a>";
+                        spantag.innerHTML += "<br>";
+                    }
+                }
+                spantag.innerHTML += "<br>Total Cost: $" + totalCost.toFixed(2);
             }
-
-            spantag.innerHTML += "Total Cost: $" + totalCost;
         } else {
             spantag.innerHTML = "";
         }
     }
 }
 
-function AddRemoveItem(action) {
-    var book = document.getElementById("book").innerHTML;
-    var isbn = document.getElementById("ISBN").innerHTML;
-    var price = document.getElementById("price").innerHTML;
+function AddRemoveItem(action, book) {
+    var bookElement = document.getElementById("book");
+    var ISBNElement = document.getElementById("ISBN");
+    var priceElement = document.getElementById("price");
 
-    if (action == "Add" || action == "Remove") {
-        xHRObject.open("GET", "test.php?action=" + action + "&book=" + encodeURIComponent(book) + "&isbn=" + encodeURIComponent(isbn) + "&price=" + encodeURIComponent(price) + "&value=" + Number(new Date), true);
-    }
+    var book = bookElement.innerHTML;
+    var ISBN = ISBNElement.innerHTML;
+    var price = parseFloat(priceElement.innerHTML.replace("$", ""));
 
+    var url = "test.php?action=" + action + "&book=" + encodeURIComponent(book) +
+        "&ISBN=" + ISBN + "&price=" + price + "&value=" + Number(new Date());
+
+    xHRObject.open("GET", url, true);
     xHRObject.onreadystatechange = getData;
     xHRObject.send(null);
 }
--- lab9/test.php
+++ lab9/test.php
@@ -3,52 +3,42 @@ session_start();
 
 $newitem = $_GET["book"];
 $action = $_GET["action"];
-$isbn = $_GET["isbn"];
-$price = $_GET["price"];
 
 if (array_key_exists("Cart", $_SESSION)) {
     $cart = $_SESSION["Cart"];
-
     if ($action == "Add") {
         if (isset($cart[$newitem])) {
-            $value = $cart[$newitem]["quantity"] + 1;
-            $cart[$newitem] = array(
-                "quantity" => $value,
-                "isbn" => $isbn,
-                "price" => $price
-            );
+            $cart[$newitem]['quantity'] += 1;
         } else {
             $cart[$newitem] = array(
-                "quantity" => "1",
-                "isbn" => $isbn,
-                "price" => $price
+                'quantity' => 1,
+                'ISBN' => $_GET['ISBN'],
+                'price' => $_GET['price']
             );
         }
-    } else {
+    } else if ($action == "Remove") {
         if (isset($cart[$newitem])) {
-            unset($cart[$newitem]);
+            $cart[$newitem]['quantity'] -= 1;
+            if ($cart[$newitem]['quantity'] <= 0) {
+                unset($cart[$newitem]);
+            }
         }
     }
 } else {
     $cart[$newitem] = array(
-        "quantity" => "1",
-        "isbn" => $isbn,
-        "price" => $price
+        'quantity' => 1,
+        'ISBN' => $_GET['ISBN'],
+        'price' => $_GET['price']
     );
 }
 
 $_SESSION["Cart"] = $cart;
 
-$totalCost = 0.0;
-
-foreach ($cart as $item) {
-    $totalCost += floatval($item["price"]) * intval($item["quantity"]);
+if (!empty($cart)) {
+    $totalCost = 0;
+    foreach ($cart as $item) {
+        $totalCost += $item['quantity'] * $item['price'];
+    }
+    echo json_encode(array('cart' => $cart, 'totalCost' => $totalCost), JSON_PRETTY_PRINT);
 }
-
-$response = array(
-    "cart" => $cart,
-    "totalCost" => $totalCost
-);
-
-echo json_encode($response, JSON_PRETTY_PRINT);
 ?>
